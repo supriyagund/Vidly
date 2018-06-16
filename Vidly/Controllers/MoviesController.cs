a9f85dec75bd7b0c466afb9ccffc20dd19968281
supriyagund11 <supriyagund11@gmail.com>
2018-06-16 21:46:03 +0900

    First commit - Display list of customers and movies

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+
+            var movie = GetMovies();
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Barfi!" },
+                new Movie { Id = 2, Name = "Minions" }
+            };
+        }
+
+    }
+}
